9d76ccbd18394f3ea2f374e8a4a5627629a680dc
Aminul-97 <amin147570bd@gmail.com>
2023-10-15 23:01:00 +0600

    Update

--- test/test_string_opt.py
+++ test/test_string_opt.py
@@ -3,28 +3,32 @@ from src.string_opt import join_string, split_string, replace_string, change_cas
 
 @pytest.fixture
 def updated_joined_text():
-    return join_string("AA", "BB")
+    # Holds the output from joined_text()
+    return join_string("This is", " a text.")
 
 @pytest.fixture
 def updated_split_string():
-    return split_string("A-B", "-")
+    # Holds the output from split_string()
+    return split_string("Text one-Text two", "-")
 
 @pytest.fixture
 def updated_replace_string():
-    return replace_string("ABC", "B", "-")
+    # Holds the output from replace_string()
+    return replace_string("Hellow world!!!", "world", "Jack")
 
 @pytest.fixture
 def updated_change_case():
-    return change_case("Abc123")
+    # Holds the output from change_case()
+    return change_case("This is a test")
 
 def test_join_string(updated_joined_text):
-    assert updated_joined_text == "AABB"
+    assert updated_joined_text == "This is a text."
 
-def test_split_string(updated_split_strin):
-    assert updated_split_string == ['A', 'B']
+def test_split_string(updated_split_string):
+    assert updated_split_string == ['Text one', 'Text two']
 
-def test_replace_string(updated_replace_strin):
-    assert updated_replace_string == "A-C"
+def test_replace_string(updated_replace_string):
+    assert updated_replace_string == "Hellow Jack!!!"
 
 def test_change_case(updated_change_case):
-    assert updated_change_case == ('ABC123', 'abc123')
+    assert updated_change_case == ('THIS IS A TEST', 'this is a test')
