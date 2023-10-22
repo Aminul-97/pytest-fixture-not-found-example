399f24b9e480ef5b0fcf99432f0f5ea082ee3c6b
Aminul-97 <amin147570bd@gmail.com>
2023-10-22 22:31:55 +0600

    Code Update

--- test/test_database_manager.py
+++ test/test_database_manager.py
@@ -7,9 +7,8 @@ def initialize_database():
     return db
 
 def initialize_database():
-    db = database_operation("127.0.0.1","root","","test")
+    db = database_operation("127.10.0.1","root","","test")
     return db
-
 
 # Testing connection
 def test_connection(initialize_database):
@@ -19,12 +18,7 @@ def test_connection(initialize_database):
 # Testing create table function
 def test_create_table(initialize_database):
     db = initialize_database
-    assert db.create_table("table3") == True
-
-# Testing get_table_data function
-def test_get_table_data(initialize_database):
-    db = initialize_database
-    assert db.get_table_data("player") != None
+    assert db.create_table("player") == True
 
 # Testing insert_data function
 def test_insert_data(initialize_database):
@@ -32,4 +26,10 @@ def test_insert_data(initialize_database):
     sql = "INSERT INTO player (name, points) VALUES (%s, %s)"
     val = ("Alex", "200")
     assert db.insert_table_data(sql, val) == True
+
+# Testing get_table_data function
+def test_get_table_data(initialize_database):
+    db = initialize_database
+    assert db.get_table_data("player") != None
+
 
