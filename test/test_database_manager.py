592e44e3d75b988cf86370aa28ee658e8f1196e5
ericsalesdeandrade <sdaeric19@gmail.com>
2023-10-24 11:16:12 +0800

    Formatting

--- test/test_database_manager.py
+++ test/test_database_manager.py
@@ -1,24 +1,29 @@
 import pytest
 from src.database_manager import database_operation
 
+
 @pytest.fixture
 def initialize_database():
-    db = database_operation("127.0.0.1","root","","test")
+    db = database_operation("127.0.0.1", "root", "", "test")
     return db
 
+
 def initialize_database():
-    db = database_operation("127.10.0.1","root","","test")
+    db = database_operation("127.10.0.1", "root", "", "test")
     return db
+
 
 # Testing connection
 def test_connection(initialize_database):
     db = initialize_database
     assert db.mydb.is_connected() == True
 
+
 # Testing create table function
 def test_create_table(initialize_database):
     db = initialize_database
     assert db.create_table("player") == True
+
 
 # Testing insert_data function
 def test_insert_data(initialize_database):
@@ -27,9 +32,8 @@ def test_insert_data(initialize_database):
     val = ("Alex", "200")
     assert db.insert_table_data(sql, val) == True
 
+
 # Testing get_table_data function
 def test_get_table_data(initialize_database):
     db = initialize_database
     assert db.get_table_data("player") != None
-
-
